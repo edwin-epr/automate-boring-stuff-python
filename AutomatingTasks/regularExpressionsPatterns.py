4a0449fadc8e022aec226a4a6efaba6828e39500
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-03-20 15:51:16 -0600

    Update learn scripting with python
    Today I learned about the use of ? and * quantifiers in regex.

--- AutomatingTasks/regularExpressionsPatterns.py
+++ AutomatingTasks/regularExpressionsPatterns.py
@@ -1,24 +1,73 @@
 # %%
 import re
+from telnetlib import SE
 # %%
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('My number is 415-555-4242.')
-print(f'Phone number found: {mo.group()}')
+if mo:
+    print(f'Phone number found: {mo.group()}')
+else:
+    print('No phone number found.')
 
 # %%
 phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 mo = phoneNumRegex.search('My number is 415-555-4242.')
-mo.group(1)
-mo.group(2)
-mo.group(0)
-mo.group()
+if mo:
+    mo.group(1)
+    mo.group(2)
+    mo.group(0)
+    mo.group()
 # %%
-mo.groups()
-areaCode, mainNumber = mo.groups()
+areaCode: str = ''
+mainNumber: str = ''
+if mo:
+    mo.groups()
+    areaCode, mainNumber = mo.groups()
 print(areaCode)
 print(mainNumber)
+
 # %%
 phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)')
 mo = phoneNumRegex.search('My number is (415) 555-4242.')
-mo.group(1)
-mo.group(2)
+if mo:
+    print(mo.group(1))
+    print(mo.group(2))
+# Matching multiple group with the pipe
+# Example A
+# %%
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+mo1.group()
+mo2 = heroRegex.search('Tina Fey and Batman')
+mo2.group()
+
+# Example B
+# %%
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
+print(mo.group(1))
+# Optional matching with the question mark
+# %%
+batRegex = re.compile(r'Bat(wo)?man')
+mo1 = batRegex.search('The adventures of Batman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+
+# %%
+phoneRegex = re.compile(r'(\d{3}-)?\d{3}-\d{4}')
+mo1 = phoneRegex.search('My number is 415-555-4242')
+print(mo1.group())
+mo2 = phoneRegex.search('My number is 555-4242')
+print(mo2.group())
+
+# Matching zero or more with the star
+# %%
+batRegex = re.compile(r'Bat(wo)*man')
+mo1 = batRegex.search('The adventures of Batman')
+print(mo1.group())
+mo2 = batRegex.search('The Adventures of Batwoman')
+print(mo2.group())
+mo3 = batRegex.search('The Adventures of Batwowowowoman')
+print(mo3.group())
